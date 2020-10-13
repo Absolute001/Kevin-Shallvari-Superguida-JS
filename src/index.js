37056c3340c3ed3c473f9b10c7bf25d3fdabdc73
Absolute001 <65250202+Absolute001@users.noreply.github.com>
2020-10-13 23:17:56 +0200

    Documentation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 
-
-
+// I vari riferimenti agli elementi della DOM utilizzati per il progetto 
 const forecastDisplay = document.querySelector(".forecastDisplay");
 const searchBtn = document.querySelector("[aria-label='SearchBtn']");
 const searchBox = document.querySelector("[aria-label='Search']");
@@ -13,7 +12,12 @@ const footer = document.querySelector("footer");
 const suggestions = document.querySelector(".suggestions");
 const cityAlert = document.querySelector(".cityAlert");
 
+//Array dei giorni della settimana 
+
 const dayName = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
+
+// creazione degli array necessari a storare le informazioni
+
 const forecast = new Array();
 const tempMax = new Array();
 const tempMin = new Array();
@@ -25,6 +29,10 @@ const wind = new Array();
 const baseURL = "http://api.openweathermap.org/data/2.5/";
 //const apiKey = process.env.API_KEY;
 const apiKey ="9dbc6ef71ee7d0814e313bbe5e2f5dcb";
+
+/*La funzione si occupa di fare la richiesta all'API sottoforma di if...else per poter
+individuare gli errori piu comuni: quando accidentalmente si preme enter e la barra di 
+ricerca risulta vuota e quando si sbaglia a digitare un luogo o questo non esiste*/
 
 function getWeather(query) {
     if (searchBox.value === "") {
@@ -41,6 +49,10 @@ function getWeather(query) {
 
     };
 }
+
+/*La funzione estrae le informazioni di cui abbiamo bisogno e le assegna ai rispettivi array che
+ad ogni richiamo vengo azzerati per poter ospitare i nuovi dati si occupa anche di tenere sempre aggiornati i giorni della settimana che visualizza attraverso
+il secondo ciclo for*/
 
 function extractInfo(weather) {
     forecast.length = 0;
@@ -65,6 +77,8 @@ function extractInfo(weather) {
     }
     displayWeather();
 }
+
+//La funziokne che si occupa di visualizzare il meteo e al contempo di gestire le animazioni ingresso/uscita tramite la rimozione e l aggiunta di classi
 
 function displayWeather() {
     home.classList.add("animate__fadeOutLeft");
@@ -97,6 +111,11 @@ function displayWeather() {
     window.scrollTo(0, 0); //Scroll on top if u are displain another city then u active geoLoc
 }
 
+//Richiama la funzione getWather() quando il pulsante "Find Me" viene premuto
+//Inserisce come parametro di getWeather la geolocalizzazione
+//Rimuove le classi di animazione che verranno re-inserite con displayWeather
+
+
 geoLoc.addEventListener("click", () =>
     navigator.geolocation.getCurrentPosition(function (position) {
         fetch(`${baseURL}forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`)
@@ -109,6 +128,11 @@ geoLoc.addEventListener("click", () =>
 
 );
 
+//Richiama la funzione getWather() quando il tasto enter viene premuto 
+//Inserisce come parametro di getWeather il valore all interno della barra di ricerca
+//Rimuove le classi di animazione che verranno re-inserite con displayWeather
+
+
 searchBox.addEventListener("keypress", function (e) {
     if (e.keyCode === 13) {
         e.preventDefault();
@@ -119,11 +143,17 @@ searchBox.addEventListener("keypress", function (e) {
     geoLoc.classList.remove("animate__fadeInUp");
 
 });
+
+//Richiama la funzione getWather() quando il pulsante submit viene premuto
+//Inserisce come parametro di getWeather il valore all interno della barra di ricerca
+//Rimuove le classi di animazione che verranno re-inserite con displayWeather
+
+
 searchBtn.addEventListener("click", function (e) {
     e.preventDefault();
     getWeather(searchBox.value);
     searchBox.value = "";
     forecastDisplay.classList.remove("animate__fadeInUp");
-    geoLoc.classList.remoxe("animate__fadeInUp");
+    geoLoc.classList.remove("animate__fadeInUp");
 
 });
